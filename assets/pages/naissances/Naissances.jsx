9741783edde8d3da8b7861a366c6efe19ebf393d
Djessa <s.djessa.2000@gmail.com>
2021-03-18 15:05:13 +0100

    Fetch data from API

--- assets/pages/naissances/Naissances.jsx
+++ assets/pages/naissances/Naissances.jsx
@@ -1,7 +1,16 @@
 import React, {Component, useState} from 'react';
 import SideMenu from "../../components/SideMenu";
-import {makeStyles, CssBaseline, createMuiTheme, ThemeProvider, Grid, Table} from '@material-ui/core';
+import {
+    CssBaseline,
+    createMuiTheme,
+    ThemeProvider,
+    Grid,
+    Table,
+    TableHead,
+    TableRow, TableCell, TableBody, withStyles, TableContainer
+} from '@material-ui/core';
 import Header from "../../components/Header";
+import axios from "axios";
 
 const theme = createMuiTheme({
     palette: {
@@ -31,64 +40,87 @@ const theme = createMuiTheme({
     }
 });
 
-const useStyles = makeStyles({
+const styles = theme => ({
     appMain: {
-      paddingLeft: '275px',
-      width: '100%'
+        paddingLeft: '275px',
+        width: '100%'
     }
 });
-const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-        field: 'age',
-        headerName: 'Age',
-        type: 'number',
-        width: 90,
-    },
-    {
-        field: 'fullName',
-        headerName: 'Full name',
-        description: 'This column has a value getter and is not sortable.',
-        sortable: false,
-        width: 160,
-        valueGetter: (params) =>
-            `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
-    },
-];
-const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
-
-export default function Naissances () {
-    const classes = useStyles();
-    const [naissances, setNaissances] = useState([]);
-    return (
-        <ThemeProvider theme={theme}>
-            <SideMenu />
-            <div className={classes.appMain}>
-                <Header> 
-                    <Grid container justify="space-between">
-                        <Grid item lg={4}><h3>Régistre des naissances</h3></Grid>
-                        <Grid item lg={8}>
-                          
-                        </Grid>
-                    </Grid>
-                </Header>
-                <div style={{ height: 400, width: '100%' }}>
-                    <Table rows={rows} columns={columns} pageSize={5} checkboxSelection />
-                </div>
-            </div>
-            <CssBaseline />
-        </ThemeProvider>
-    );
+const getAll = () => {
+    let data;
+    axios.get('/births')
+        .then((response) => {
+            data = response.data;
+        })
+        .catch((error) => {
+            data = [];
+        });
+    return data;
 }
+
+class Naissances extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fiches: []
+        }
+        this.readFiches();
+    }
+    readFiches() {
+        axios.get('/births')
+            .then((response) => {
+                this.setState(
+                    {fiches: response.data}
+                );
+            })
+            .catch((error) => {
+                console.log(error)
+            });
+    }
+    render() {
+        const {classes} = this.props;
+        return (
+            <ThemeProvider theme={theme}>
+                <SideMenu/>
+                <div className={classes.appMain}>
+                    <Header>
+                        <Grid container justify="space-between">
+                            <Grid item lg={4}><h3>Régistre des naissances</h3></Grid>
+                            <Grid item lg={8}>
+                            </Grid>
+                        </Grid>
+                    </Header>
+                    <TableContainer>
+                        <Table stickyHeader aria-label="sticky table">
+                            <TableHead color="primary">
+                                <TableRow style={{fontWeight: 'bold'}}>
+                                    <TableCell>Nom</TableCell>
+                                    <TableCell>Prénom</TableCell>
+                                    <TableCell>Sexe</TableCell>
+                                    <TableCell>Date de naissance</TableCell>
+                                    <TableCell>Lieu de naissance</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {this.state.fiches.map((data, index) => {
+                                    let fiche = JSON.parse(data);
+                                    let person = JSON.parse(fiche.enfant);
+                                    return<TableRow key={index}>
+                                        <TableCell>{person.first_name}</TableCell>
+                                        <TableCell>{person.last_name}</TableCell>
+                                        <TableCell>{person.sexe}</TableCell>
+                                        <TableCell>{person.birthdate}</TableCell>
+                                        <TableCell>{person.birthplace}</TableCell>
+                                    </TableRow>
+                                })}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </div>
+                <CssBaseline/>
+            </ThemeProvider>
+        );
+    }
+}
+
+export default withStyles(styles) (Naissances);
