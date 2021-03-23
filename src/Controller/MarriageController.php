1e936edcc200c0bc47c276498afbc50214a93840
Djessa <s.djessa.2000@gmail.com>
2021-03-23 12:56:50 +0100

    refactoring

--- src/Controller/MarriageController.php
+++ src/Controller/MarriageController.php
@@ -1,14 +1,15 @@
 <?php
 
 namespace App\Controller;
-use App\Repository\PersonRepository;
 use App\Entity\Marriage;
-use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\HttpFoundation\Request;
+use App\Service\PersonSerivce;
+use App\Repository\PersonRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
  /**
  * @Route("/marriage")
  */
@@ -25,70 +26,46 @@ class MarriageController extends AbstractController
     /**
      * @Route("/new", name="marriage_new")
      */
-    public function new(Request $request, SerializerInterface $serializer, PersonRepository $persons)
+    public function new(PersonSerivce $personnes, Request $request, SerializerInterface $serializer, PersonRepository $persons)
     {
     	try {
     		$marriage = $serializer->deserialize($request->getContent(), Marriage::class, 'json');
 		} catch (NotEncodableValueException $e) {
             return $this->json(['status' => 400, 'message' => $e->getMessage()]);
         }
-		try { 	
-			$man_cle = [
-    			'first_name' => $marriage->getMan()->getFirstName(), 
-    			'last_name' =>  $marriage->getMan()->getLastName(),
-    			'birthdate' =>  $marriage->getMan()->getBirthdate(),
-    			'birthplace' => $marriage->getMan()->getBirthplace()
-    		];
-    		if (!$persons->findOneBy($man_cle)) {
+		try { 
+			// Man property
+			$man = $personnes->isExist($marriage->getMan());
+    		if ($man) {
+				$marriage->setMan($man);
+    		} else {
     			$this->manager->persist($marriage->getMan());
-    		} else {
-    			$man = $persons->findOneBy($man_cle);
-    			$marriage->getMan()->setId($man->getId());
-    			$marriage->getMan()->setSexe($man->getSexe());
     		}
-    		 $woman_cle = [
-    			'first_name' => $marriage->getWoman()->getFirstName(), 
-    			'last_name' =>  $marriage->getWoman()->getLastName(),
-    			'birthdate' =>  $marriage->getWoman()->getBirthdate(),
-    			'birthplace' => $marriage->getWoman()->getBirthplace()
-    		];
-    		if (!$persons->findOneBy($woman_cle)) {
-    			$this->manager->persist($marriage->getWoman());
+			// Woman property
+			$woman = $persons->findOneBy($marriage->getWoman());
+    		if ($woman) {
+				$marriage->setWoman($woman);
     		} else {
-    			$woman = $persons->findOneBy($woman_cle);
-    			$marriage->getWoman()->setId($woman->getId());
-    			$marriage->getWoman()->setSexe($woman->getSexe());
+				$this->manager->persist($marriage->getWoman());
     		}
-    		$man_cle = [
-    			'first_name' => $marriage->getWitnessMan()->getFirstName(), 
-    			'last_name' =>  $marriage->getWitnessMan()->getLastName(),
-    			'birthdate' =>  $marriage->getWitnessMan()->getBirthdate(),
-    			'birthplace' => $marriage->getWitnessMan()->getBirthplace()
-    		];
-    		if (!$persons->findOneBy($man_cle)) {
+			// WitnessMan property
+			$witnessMan = $persons->findOneBy($marriage->getWitnessMan());
+    		if ($witnessMan) {
+				$marriage->setWitnessMan($witnessMan);
+    		} else {
     			$this->manager->persist($marriage->getWitnessMan());
-    		} else {
-    			$man = $persons->findOneBy($man_cle);
-    			$marriage->getWitnessMan()->setId($man->getId());
-    			$marriage->getWitnessMan()->setSexe($man->getSexe());
     		}
-    		$man_cle = [
-    			'first_name' => $marriage->getWitnessWoman()->getFirstName(), 
-    			'last_name' =>  $marriage->getWitnessWoman()->getLastName(),
-    			'birthdate' =>  $marriage->getWitnessWoman()->getBirthdate(),
-    			'birthplace' => $marriage->getWitnessWoman()->getBirthplace()
-    		];
-    		if (!$persons->findOneBy($man_cle)) {
-    			$this->manager->persist($marriage->getWitnessWoman());
+			// WitnessWoman property
+			$witnessWoman = $persons->findOneBy($marriage->getWitnessWoman());
+    		if ($witnessWoman) {
+    			$marriage->setWitnessWoman($witnessWoman);
     		} else {
-    			$man = $persons->findOneBy($man_cle);
-    			$marriage->getWitnessWoman()->setId($man->getId());
-    			$marriage->getWitnessWoman()->setSexe($man->getSexe());
+				$this->manager->persist($marriage->getWitnessWoman());
     		}
     		$marriage->setCreatedAt(new \DateTime());
     		$this->manager->persist($marriage);
     		$this->manager->flush();
-    		return $this->json(['message' => 'Une acte de mariage a bien été crée'], 200);
+    		return $this->json(['message' => 'Enregistré avec succès'], 200);
     	} catch (Exception $e) {
             return $this->json(['status' => 400, 'message' => $e->getMessage()]);
         } 	
