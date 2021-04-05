d4e36e709283146bd2a606358a5b51bd47f86714
Djessa <s.djessa.2000@gmail.com>
2021-04-05 16:31:11 +0100

    Mise à jour de la structure de classe naissance et personne

--- assets/class/Naissance.js
+++ assets/class/Naissance.js
@@ -4,12 +4,10 @@ export class Naissance {
 
 	constructor() {
 		this.date_declaration = currentDate
-		this.heure_declaration = currentTime
 		this.type_declaration = ''
 		this.numero_jugement = ''
 		this.date_jugement = ''
-		this.date_naissance = ''
+		this.heure_declaration = currentTime
 		this.heure_naissance = ''
-		this.lieu_naissance = ''
 	}
 }
--- assets/class/Personne.js
+++ assets/class/Personne.js
@@ -3,16 +3,11 @@ export class Personne {
 	constructor() {
 		this.nom = ''
 		this.prenom = ''
+		this.date_naissance = ''
+		this.lieu_naissance = ''
 		this.sexe = ''
 		this.profession = ''
 		this.residence = ''
 	}
 
-}
-
-export class Adult extends Personne {
-	constructor() {
-		super()
-		this.naissance = { date_naissance: '', lieu_naissance: '' }
-	}
 }
